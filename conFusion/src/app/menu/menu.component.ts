fb6fbbed3a24704fa0754de129243ef21a16f47c
Jorge Espina <canchemolinas@gmail.com>
2020-07-17 15:31:42 -0600

    Header and footer

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
+import { Dish } from '../menu/shared/dish';
 import { DishService } from '../services/dish.service';
 
 @Component({
