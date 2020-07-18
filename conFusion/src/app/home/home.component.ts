6e32651237b52d46023a1872698b272ca4ca8e95
Jorge Espina <canchemolinas@gmail.com>
2020-07-18 15:25:13 -0600

    Single Page Applications Part 2

--- conFusion/src/app/home/home.component.ts
+++ conFusion/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../menu/shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../menu/shared/promotion';
-import { PromotionService } from '../services/promotionservice.service';
+import { PromotionService } from '../services/promotion.service';
 
 @Component({
   selector: 'app-home',
