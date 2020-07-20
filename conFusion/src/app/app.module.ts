31d227de1788365a0e0d3c02f266ad8414c9bb6e
Jorge Espina <canchemolinas@gmail.com>
2020-07-20 16:23:43 -0600

    Directives

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { ProcessHttpmsgService } from './services/process-httpmsg.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './menu/shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { baseURL } from './menu/shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
